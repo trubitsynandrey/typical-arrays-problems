bcc3d9d03a8cd3f7d037d3b6439dd43ccad904b3
Andrew <iverstoon@icloud.com>
2021-03-05 15:27:33 +0400

    feat: solution is ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    }
+    let len = array.length,
+        minimum = Infinity;
+    while (len--) {
+        if (array[len] < minimum) {
+            minimum = array[len];
+        }
+    }
+    return minimum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    }
+    let len = array.length,
+        maximum = -Infinity;
+    while (len--) {
+        if (array[len] > maximum) {
+            maximum = array[len];
+        }
+    }
+    return maximum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    }
+    let len = array.length,
+        sum = 0;
+    for (i = 0; i < len; i++) {
+        sum += array[i];
+    }
+    let average = sum / len;
+    return average;
 }
